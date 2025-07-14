a9f263a99b7e0d1178d1f71528e46a05ca8bb2fb
Mark Baggett <markpbaggett@gmail.com>
2025-07-14 08:13:06 -0500

    PAJ and AAVPT should work.

--- src/ojsnake.py
+++ src/ojsnake.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import yaml
 from csv import DictWriter
 from lxml import etree
@@ -10,6 +9,7 @@ import os
 from bs4 import BeautifulSoup as bs
 from pdf2image import convert_from_path
 import argparse
+import json
 
 
 class Article:
@@ -57,23 +57,25 @@ class Article:
             source = self.tree.find(
                 ".//dc:source", namespaces=self.namespaces
             )
-        # try:
+            subjects = self.tree.find(
+                ".//dc:subject", namespaces=self.namespaces
+            )
+
         all_galleys = self.all_data['publications'][0]['galleys']
+        final_galley = None
         for galley in all_galleys:
             request_link = galley['urlPublished'], "application/pdf"
             content_type = requests.get(galley['urlPublished']).headers.get('content-type')
             if "application/pdf" not in content_type:
                 request_link = galley['file']['url'], content_type
             r = requests.get(request_link[0])
-            if r.status_code == 200:
+            if r.status_code == 200 and galley["urlRemote"] != "RemoteURL":
                 published_galley = request_link
+                final_galley = galley
                 break
             else:
                 print(r.status_code)
                 print('Galley Not Published! Trying another.')
-        # except:
-        #     print('No Galley Found!')
-        #     published_galley = None
         if published_galley:
             original = published_galley
         else:
@@ -83,7 +85,7 @@ class Article:
                 original = self.all_data['publications'][0]['urlPublished'], "missing"
             except KeyError:
                 original = "", ""
-        bundles = self.get_bundles(original)
+        bundles = self.get_bundles(original, final_galley)
         return {
             "bundle:ORIGINAL": bundles["original"],
             "bundle:THUMBNAIL": bundles["thumbnail"],
@@ -96,12 +98,28 @@ class Article:
             "relation.isJournalIssueOfPublication": ""
         }
 
-    def get_bundles(self, current):
+    def get_bundles(self, current, galley):
         if current[1] == "application/pdf":
             return {
                 "original": current[0],
                 "thumbnail": self.get_thumbnail(current[0], self.base_thumb),
             }
+        
+        elif "text/html" in current[1]:
+            with open('review.json', 'w') as quick_test:
+                json.dump(self.all_data, quick_test, indent=4)
+            
+            first = galley['file']["submissionId"]
+            second = galley['file']["assocId"]
+            third = galley['file']['id']
+            base = galley["urlPublished"].split('/')[2]
+            sub = galley["urlPublished"].split('/')[3]
+            full_url = f"https://{base}/{sub}/article/download/{first}/{second}/{third}"
+            return {
+                "original": full_url,
+                "thumbnail": self.get_thumbnail(full_url, self.base_thumb),
+            }
+            
         else:
             if not os.path.exists(self.output_dir):
                 os.makedirs(self.output_dir)
@@ -110,16 +128,21 @@ class Article:
             if not os.path.exists(f"{self.output_dir}/thumbnails"):
                 os.makedirs(f"{self.output_dir}/thumbnails")
             r = requests.get(current[0], stream=True)
-            original_filename = r.headers.get('content-disposition').split('attachment;filename="')[1].split('"')[0]
+            try:
+                original_filename = r.headers.get('content-disposition').split('attachment;filename="')[1].split('"')[0]
+            except AttributeError:
+                print(r.headers)
+                print(current)
+                original_filename = "unknown.pdf"
             with open(f"{self.output_dir}/originals/{original_filename}", 'wb') as f:
-                for chunk in response.iter_content(chunk_size=8192):
+                for chunk in r.iter_content(chunk_size=8192):
                     if chunk:  # filter out keep-alive chunks
                         f.write(chunk)
-            images = convert_from_path(f'{current_dir}/originals/{original_filename}', first_page=1, last_page=1)
-            images[0].save(f'{current_dir}/thumbnails/{original_filename.replace("pdf", ".jpg")}', 'JPEG')
+            images = convert_from_path(f'{self.output_dir}/originals/{original_filename}', first_page=1, last_page=1)
+            images[0].save(f'{self.output_dir}/thumbnails/{original_filename.replace("pdf", ".jpg")}', 'JPEG')
             return {
                 'original': f"{self.output_dir}/originals/{original_filename}",
-                'thumbnail': f'{current_dir}/thumbnails/{original_filename.replace("pdf", ".jpg")}'
+                'thumbnail': f'{self.output_dir}/thumbnails/{original_filename.replace("pdf", ".jpg")}'
             }
 
     @staticmethod
@@ -182,7 +205,10 @@ class OJSnake:
         all_issues = self.get_issues()
         all_articles = []
         for issue in all_issues.get('items', []):
-            issue_articles = self.get_articles(issue.get("id"), issue.get("identification"))
+            issue_articles = self.get_articles(
+                issue.get("id"), 
+                issue.get("identification")
+            )
             for article in issue_articles:
                 all_articles.append(article.for_csv)
         return all_articles
@@ -240,7 +266,7 @@ class OJSnake:
 
     def get_title_data(self):
         return {
-            "bundle:THUMBNAIL": self.journal_config.get('default_thumbnail', ''),
+            "bundle:THUMBNAIL": self.journal_config.get('default_thumbnail', "fixtures/default.png"),
             "dc.title": self.journal_title,
             "dc.date": self.journal_config.get('date', ''),
             "dc.subject": ",".join(self.journal_config.get('subjects', [])),
@@ -256,6 +282,9 @@ class OJSnake:
             writer.writeheader()
             writer.writerow(title)
 
+    def get_reviewers(self):
+        r = requests.get(f"{self.url}/api/v1/users/reviewers", headers=self.headers)
+        return r.json()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
